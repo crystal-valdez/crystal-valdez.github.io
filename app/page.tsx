8c571df33eacf788a77744f48a2d7e004aa99bdc
Crystal Valdez <crystal@crystals-air.home>
2024-03-07 01:04:09 -0500

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
 import About from "@/components/about";
-import Contact from "@/components/contact";
-import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
@@ -12,10 +10,7 @@ export default function Home() {
       <Intro />
       <SectionDivider />
       <About />
-      <Projects />
       <Skills />
-      <Experience />
-      <Contact />
     </main>
   );
 }
